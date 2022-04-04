f05f848c8f133ea860b0d4a014d38f72856e94e4
Ernilson Daniel <ernilsondaniel@sisconsultoria.com.br>
2022-04-04 10:39:02 -0300

    cria database

--- src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
+++ src/main/resources/db/migration/V8__Insert_Data_In_Users.sql
@@ -1,3 +1,3 @@
 ﻿INSERT INTO `users` (`user_name`, `full_name`, `password`, `account_non_expired`, `account_non_locked`, `credentials_non_expired`, `enabled`) VALUES
 	('Ernilson', 'Ernilson Souza', '$2a$10$BRO4Ky0ODRl5eiSJCNfWIu4eA.Au48RCaojhtzDjbz7nxZ9691pVu', b'1', b'1', b'1', b'1'),	
-	('teste', 'teste teste', '$2a$16$9qr2tv0HmXbHBsx.TZFjfux742wCZM32a8Wi6iBqwIqaizlHPuxHS', b'1', b'1', b'1', b'1');
+	('teste', 'teste teste', '$2a$10$8aM3TWao8vPNyiBFeD/xxuKAydkdC6RJ97VHMIPtNWeTAjfX83Ise', b'1', b'1', b'1', b'1');
